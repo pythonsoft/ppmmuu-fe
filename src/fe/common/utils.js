8213c88ce33cca50f55eb8a8a8c043b1b8890064
steven <1035968349@qq.com>
2017-07-24 16:12:07 +0800

    remove axios from utils and add it to api

--- src/fe/common/utils.js
+++ src/fe/common/utils.js
@@ -1,37 +1,4 @@
-import axios from 'axios';
-
 const utils = {};
-
-axios.defaults.withCredentials = true;
-
-axios.interceptors.request.use((config) => {
-  // Do something before request is sent
-  if (config.method === 'get') {
-    config.params = config.params || {};
-    config.params.t = new Date().getTime();
-  } else if (config.method === 'post') {
-    config.data = config.data || {};
-    config.data.t = new Date().getTime();
-  }
-
-  return config;
-}, error =>
-  // Do something with request error
-  Promise.reject(error)
-);
-
-axios.interceptors.response.use((response) => {
-  // Do something with response data
-  const res = response.data;
-  const loginStatusCodeArr = ['-3001', '-3002', '-3003', '-3004'];
-  if (loginStatusCodeArr.indexOf(res.status) !== -1) {
-    window.location.href = '/login';
-  }
-  return response;
-}, error =>
-  // Do something with response error
-  Promise.reject(error)
-);
 
 utils.formatQuery = function formatQuery(obj, isGet = false) {
   return isGet ? {
