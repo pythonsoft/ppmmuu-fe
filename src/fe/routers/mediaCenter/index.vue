de4887758cf33ed1cfdd3454ca2a73011fb93f27
steven <1035968349@qq.com>
2017-08-03 16:32:47 +0800

    fix

--- src/fe/routers/mediaCenter/index.vue
+++ src/fe/routers/mediaCenter/index.vue
@@ -235,7 +235,7 @@
       getShortName(item) {
         let name = item.program_name_en || '';
         if(name && name.length > 15) {
-          name = name.substr(0, 10) + '...';
+          name = name.substr(0, 15) + '...';
         }
         return name;
       }
