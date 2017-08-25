77a20df910d382c100d06722ed0ebeb33943535d
chaoningx <chaoningx@phoenixtv.com>
2017-08-25 18:35:37 +0800

    Merge branch 'master' of http://gitlab.szdev.cn/dev/UMP-FE
    # Conflicts:
    #	src/fe/routers/movieEditor/index.vue

--- src/fe/api/media.js
+++ src/fe/api/media.js
@@ -115,4 +115,26 @@ api.getVideo = function getVideo(data, scope) {
   });
 };
 
+api.getStream = function getStream(data, scope) {
+  return new Promise((resolve, reject) => {
+    if (scope) { scope.$progress.start(); }
+    axios.get('/media/getStream', data).then((response) => {
+      if (!response) {
+        reject('返回数据格式不正确');
+        return false;
+      }
+      const res = response.data;
+      if (res.status === '0') {
+        if (scope) { scope.$progress.finish(); }
+        return resolve(res);
+      }
+      if (scope) { scope.$progress.fail(); }
+      return reject(res.statusInfo.message);
+    }).catch((error) => {
+      if (scope) { scope.$progress.fail(); }
+      reject(error);
+    });
+  });
+};
+
 module.exports = api;
