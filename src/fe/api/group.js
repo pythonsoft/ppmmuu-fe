c6324972a1ef5ccc863667fb659fee929da46b4a
steven <1035968349@qq.com>
2017-09-16 20:03:52 +0800

    fix

--- src/fe/api/group.js
+++ src/fe/api/group.js
@@ -353,4 +353,26 @@ api.getGroupSearchUser = function getGroupSearchUser(data, scope) {
   });
 };
 
+api.postBindMediaExpressUser = function postBindMediaExpressUser(data, scope) {
+  return new Promise((resolve, reject) => {
+    if (scope) { scope.$progress.start(); }
+    axios.post('/group/bindMediaExpressUser', data).then((response) => {
+      if (!response) {
+        reject('返回数据格式不正确');
+        return false;
+      }
+      const res = response.data;
+      if (res.status === '0') {
+        if (scope) { scope.$progress.finish(); }
+        return resolve(res);
+      }
+      if (scope) { scope.$progress.fail(); }
+      return reject(res.statusInfo.message);
+    }).catch((error) => {
+      if (scope) { scope.$progress.fail(); }
+      reject(error);
+    });
+  });
+};
+
 module.exports = api;
--- src/fe/routers/management/account/component/accountList.vue
+++ src/fe/routers/management/account/component/accountList.vue
@@ -29,6 +29,7 @@
         <fj-button size="mini" type="primary" :disabled="selectedItems.length<=0" @click="showUserStatusDialog('enable')">启用</fj-button>
         <fj-button size="mini" type="primary" :disabled="selectedItems.length<=0" @click="showUserStatusDialog('disabled')">禁用</fj-button>
         <fj-button size="mini" type="primary" :disabled="selectedItems.length<=0" @click="showUserStatusDialog('delete')">删除</fj-button>
+        <fj-button size="mini" type="primary" :disabled="selectedItems.length!=1" @click="showBindMediaExpressDialog">绑定快传账户</fj-button>
       </div>
     </template>
     <template slot="table">
@@ -63,6 +64,13 @@
           @updateList="updateList"></edit-user-content>
     </fj-slide-dialog>
     <fj-slide-dialog
+            title="绑定快传账户"
+            :visible.sync="bindMediaExpressDialogVisible">
+            <bind-media-express
+              :id="bindMediaExpressDialogId"
+              @cancel="bindMediaExpressDialogVisible=false"></bind-media-express>
+    </fj-slide-dialog>
+    <fj-slide-dialog
       title="修改权限信息"
       :visible.sync="permissionDialogVisible">
         <permission-content
@@ -92,6 +100,7 @@
   import FourRowLayoutRightContent from '../../../../component/layout/fourRowLayoutRightContent';
   import EditUserContent from './editUserContent';
   import PermissionContent from './permissionContent';
+  import BindMediaExpress from './bindMediaExpress';
   import groupAPI from '../../../../api/group';
   import { formatQuery } from '../../../../common/utils';
   import AddGroup from '../../role/searchAddGroup';
@@ -123,7 +132,9 @@
         editUserStatusDialogTitle: '删除成员',
         editUserStatusIds: [],
         isEditUserStatusBtnLoading: false,
-        editUserStatusFn: () => {}
+        editUserStatusFn: () => {},
+        bindMediaExpressDialogVisible: false,
+        bindMediaExpressDialogId: ''
       };
     },
     created() {
@@ -208,6 +219,10 @@
           selectedItem.company._id
         ];
       },
+      showBindMediaExpressDialog() {
+        this.bindMediaExpressDialogVisible = true;
+        this.bindMediaExpressDialogId = this.selectedItems[0]._id;
+      },
       showUserStatusDialog(type) {
         this.editUserStatusDialogVisible = true;
         this.editUserStatusDialogTitle = this.EDIT_USER_STATUS_CONFIG[type].text;
@@ -266,7 +281,8 @@
       FourRowLayoutRightContent,
       EditUserContent,
       PermissionContent,
-      AddGroup
+      AddGroup,
+      BindMediaExpress
     }
   };
 </script>
