56c6c82e8a42e39a076456b432ab866fbd426210
steven <1035968349@qq.com>
2017-09-22 12:30:08 +0800

    fix 标识不能更新

--- src/fe/routers/management/template/download/component/download.vue
+++ src/fe/routers/management/template/download/component/download.vue
@@ -2,7 +2,7 @@
   <div>
   <fj-form :model="formData" :rules="rules" ref="editForm" label-width="90px">
     <fj-form-item label="标志">
-      <fj-input v-model="formData.id"></fj-input>
+      <fj-input v-model="formData.id" :disabled="type==='update'"></fj-input>
     </fj-form-item>
     <fj-form-item label="名称" prop="name">
       <fj-input v-model="formData.name"></fj-input>
