1475eaaadd6c68b85bd7dab8b18aa1aaefbaeb34
steven <1035968349@qq.com>
2017-10-19 15:29:50 +0800

    fix audit

--- src/fe/component/higherOrder/bubble/index.js
+++ src/fe/component/higherOrder/bubble/index.js
@@ -1,11 +1,11 @@
-if(!window._ump_store) {
+if (!window._ump_store) {
   window._ump_store = {};
 }
 
 const bubble = {};
 
 bubble.on = function (eventName, fn) {
-  if(!window._ump_store[eventName]) {
+  if (!window._ump_store[eventName]) {
     window._ump_store[eventName] = [];
   }
 
@@ -13,26 +13,26 @@ bubble.on = function (eventName, fn) {
 };
 
 bubble.emit = function (eventName) {
-  if(window._ump_store[eventName] && window._ump_store[eventName].length > 0) {
+  if (window._ump_store[eventName] && window._ump_store[eventName].length > 0) {
     let params = [];
 
-    if(arguments && arguments.length > 0) {
+    if (arguments && arguments.length > 0) {
       params = Array.prototype.slice.call(arguments);
       params.shift();
     }
 
-    for(let i = 0; i < window._ump_store[eventName].length; i++) {
+    for (let i = 0; i < window._ump_store[eventName].length; i++) {
       window._ump_store[eventName][i] && window._ump_store[eventName][i].apply(window._ump_store[eventName][i], params);
     }
   }
 };
 
 bubble.off = function (eventName, fn) {
-  if(window._ump_store[eventName] && window._ump_store[eventName].length > 0) {
-    if(!fn) {
+  if (window._ump_store[eventName] && window._ump_store[eventName].length > 0) {
+    if (!fn) {
       window._ump_store[eventName] = [];
-    }else {
-      for (let  i = 0, n = 0; i < window._ump_store[eventName].length; i ++) {
+    } else {
+      for (let i = 0, n = 0; i < window._ump_store[eventName].length; i++) {
         if (window._ump_store[eventName][i] !== fn) {
           window._ump_store[eventName][n++] = window._ump_store[eventName][i];
         }
