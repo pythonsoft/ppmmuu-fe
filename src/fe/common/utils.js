ddc470af05c3d7db7400e12f094398bd99d12be2
chaoningx <chaoningx@phoenixtv.com>
2017-10-24 11:21:18 +0800

    播放路径变更

--- src/fe/common/utils.js
+++ src/fe/common/utils.js
@@ -487,10 +487,15 @@ utils.getSRT = function (objectId, cb, scope, fps = 25) {
 
 // let t = 0;
 
+function formatFileExtToMp4(fileName) {
+  const name = fileName.replace('.wmv', '.mp4');
+  return name;
+}
+
 utils.getStreamURL = function getStreamURL(objectId, cb, scope) {
   mediaAPI.getStream({ params: { objectid: objectId } }, scope).then((res) => {
-    let dateString = res.result.UNCPATH;
-    const fileName = res.result.FILENAME;
+    let dateString = res.result.UNCPATH || '';
+    let fileName = res.result.FILENAME || '';
 
     if (dateString) {
       dateString = dateString.replace('\\', '\\\\').match(/\\\d{4}\\\d{2}\\\d{2}/g);
@@ -504,7 +509,24 @@ utils.getStreamURL = function getStreamURL(objectId, cb, scope) {
       * OUTPOINT: 750
       * UNCPATH: "U:\MAM\low1\2017\10\16"
       * */
-      const url = `${config.defaults.streamURL + dateString}/${fileName}`;
+      const dateArray = dateString.split('/');
+      const year = dateArray[1] * 1;
+      const month = dateArray[2] * 1;
+      const day = dateArray[3] * 1;
+      let playPath = '/u';
+
+      //2012/9/18
+
+      if(year < 2012) {
+        playPath = '/y';
+        fileName = formatFileExtToMp4(fileName);
+      }else if(year === 2012 || (year === 2013 && month <= 2 && day <= 28)) {
+        playPath = '/w';
+        fileName = formatFileExtToMp4(fileName);
+      }
+
+      const url = `${config.defaults.streamURL}${playPath}${dateString}/${fileName}`;
+
       // if (config.defaults.streamURL === 'http://localhost:8080' || config.defaults.streamURL === 'http://api.szdev.cn') {
       //   if (t % 2 === 0) {
       //     url = '/static/video/test.mp4';
