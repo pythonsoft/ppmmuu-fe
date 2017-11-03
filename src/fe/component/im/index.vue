06a17438594585037b4a56f973804437939eaa16
chaoningx <chaoningx@phoenixtv.com>
2017-11-03 18:52:47 +0800

    添加 im样式及更新icon文件

--- src/fe/component/im/index.vue
+++ src/fe/component/im/index.vue
@@ -1,6 +1,37 @@
 <template>
-  <div class="im-main">
-    sss
+  <div>
+
+    <div class="im-main" v-show="!dialogVisible" @click="displayDialog">
+      <span v-if="message">xxx</span>
+      <span v-else class="iconfont icon-jiqiren im-xiaou-icon"></span>
+    </div>
+
+    <div v-show="dialogVisible" class="im-dialog">
+      <div class="im-dialog-wrap">
+        <div class="im-dialog-bar">
+          <span class="iconfont icon-jiqiren im-dialog-logo"></span>
+          <ul class="im-dialog-bar-action">
+            <li @click="closeHandle">
+              <span class="iconfont icon-small-close im-dialog-close"></span>
+            </li>
+          </ul>
+        </div>
+        <div class="im-dialog-main">
+          <div class="im-dialog-main-left">
+            <div class="im-main-left-search"></div>
+            <div class="im-main-left-dialog-list"></div>
+          </div>
+          <div class="im-dialog-main-right">
+            <div class="im-dialog-main-right-bar"></div>
+            <div class="im-dialog-main-right-content"></div>
+            <div class="im-dialog-main-right-chat">
+              <div class="im-dialog-main-right-chat-bar"></div>
+              <div class="im-dialog-main-right-chat-input"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -10,11 +41,17 @@
     name: 'im',
     data() {
       return {
-
+        message: null,
+        dialogVisible: false,
       }
     },
     methods: {
-
+      displayDialog() {
+        this.dialogVisible = true;
+      },
+      closeHandle() {
+        this.dialogVisible = false;
+      }
     }
   }
 </script>
