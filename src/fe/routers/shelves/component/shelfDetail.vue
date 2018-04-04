744d7063cc3a4bb8ee7841922b4985083c525e96
luoting <luoting@phoenixtv.com>
2018-04-04 17:03:32 +0800

    update program origin field

--- src/fe/routers/shelves/component/shelfDetail.vue
+++ src/fe/routers/shelves/component/shelfDetail.vue
@@ -231,9 +231,10 @@
         const me = this;
         api.getObject(formatQuery({ objectid: me.objectId, fromWhere: me.fromWhere }, true))
           .then((res)=>{
-            const data = res.data.result.detail.program;
-            me.programDetails = data;
-            me.programGroup = formatProgramGroup(data);
+            const detail = res.data.result.detail;
+            const program = detail.program.length > 0 ? detail.program : detail.sequence;
+            me.programDetails = program;
+            me.programGroup = formatProgramGroup(program);
             if (isEmptyObject(res.data.result.detail.program)) {
               me.programDetails = res.data.result.detail.sequence;
             }
